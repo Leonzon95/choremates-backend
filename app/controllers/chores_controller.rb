5036f8bc25a507efe629aa7f34a0b77b4016c9f5
leobr <leobrionesherrera@gmail.com>
2020-10-21 22:07:26 -0500

    Chores can be updated and deleted with websocket

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -14,6 +14,7 @@ class ChoresController < ApplicationController
     def update
         house = House.find_by_id(params[:house_id])
         chore = Chore.find_by_id(params[:id])
+        is_old = !!chore.house_member
         if chore.update(chore_params)
             if params[:houseMemberId]
                 house_member = HouseMember.all.find_by_id(params[:houseMemberId])
@@ -21,6 +22,9 @@ class ChoresController < ApplicationController
                 chore.save
             end
             obj = {chore: ChoreSerializer.new(chore), action: 'edit'}
+            if params[:houseMemberId] && !is_old 
+                obj[:isAssg] = true
+            end
             HouseChannel.broadcast_to(house, obj)
             # render json: {chore: chore}
         else
@@ -29,8 +33,11 @@ class ChoresController < ApplicationController
     end
 
     def destroy
+        house = House.find_by_id(params[:house_id])
         chore = Chore.find_by_id(params[:id])
         chore.destroy
+        obj = {id: params[:id], action: 'delete'}
+        HouseChannel.broadcast_to(house, obj)
     end
 
     private
